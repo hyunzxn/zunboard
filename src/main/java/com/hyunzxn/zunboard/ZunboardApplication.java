513ff44490b710826105d9cdfc54bee53dd78346
hyunzxn <jann1653@gmail.com>
2023-03-08 09:45:21 +0900

    initial commit

---
+++ src/main/java/com/hyunzxn/zunboard/ZunboardApplication.java
@@ -0,0 +1,13 @@
+package com.hyunzxn.zunboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZunboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZunboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyunzxn/zunboard/ZunboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyunzxn.zunboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZunboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
