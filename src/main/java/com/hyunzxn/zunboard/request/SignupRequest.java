034e6b026863527d52ca3bf3f418cf586e68d5d8
hyunzxn <jann1653@gmail.com>
2023-03-10 11:40:41 +0900

    @NoArgsConstructor 어노테이션 추가

--- src/main/java/com/hyunzxn/zunboard/request/SignupRequest.java
+++ src/main/java/com/hyunzxn/zunboard/request/SignupRequest.java
@@ -2,8 +2,10 @@ package com.hyunzxn.zunboard.request;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class SignupRequest {
 
 	private String username;
