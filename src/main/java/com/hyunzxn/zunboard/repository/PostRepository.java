2ea005eb8780c06f976943647fea484a545bb395
hyunzxn <jann1653@gmail.com>
2023-03-15 12:25:02 +0900

    PostRepository findById 메서드 중복되던 것 삭제

--- src/main/java/com/hyunzxn/zunboard/repository/PostRepository.java
+++ src/main/java/com/hyunzxn/zunboard/repository/PostRepository.java
@@ -13,10 +13,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
 	Page<Post> findAllByOrderByIdDesc(Pageable pageable);
 
-	// @Override
-	// @Query("select p from Post p join fetch p.user where p.id =:id")
-	// Optional<Post> findById(@Param("id") Long id);
-
 	@Override
 	@EntityGraph(attributePaths = {"user"})
 	Optional<Post> findById(Long id);
